6fc41510facf00048199c137662b5f34e92cc9e6
Lee, juhyung <p99265407@gmail.com>
2025-10-19 20:33:17 +0900

    five

--- src/f2222141111/World.java
+++ src/f2222141111/World.java
@@ -1,8 +1,12 @@
 package f2222141111;
 
 public class World{
+
     public static void run(){
         System.out.println("202214062");
+    }
+    public static void run2(){
+        System.out.println("hello world");
     }
 }
 
